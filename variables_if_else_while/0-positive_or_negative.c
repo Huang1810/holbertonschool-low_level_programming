94bcd12eb5f9002a836d78ab485a883e9ebd6ab3
Huang1810 <ahhamed1810@gmail.com>
2024-02-26 12:15:07 +0100

    commit

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void)
+int
+{
+    if (n < 0)
+    {
+        printf("%d is negatif", n);
+    } else if (n > 0) {
+        printf("%d is positif", n);
+    } else {
+        printf("%n is zero", n)
+    }
+}
