33f743156c33ce97cab8aee7f14a59728967fca2
Huang1810 <ahhamed1810@gmail.com>
2024-02-28 11:35:56 +0100

    commit

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,11 +1,12 @@
 #include"main.h"
 /**
-* _mul - multiplies two integers.
+* mul - multiplies two integers.
 * @x: multiple
 * @y: multiple
 * Return: void.
 */
 int mul(int x, int y)
+
 {
 return (x * y);
 }
