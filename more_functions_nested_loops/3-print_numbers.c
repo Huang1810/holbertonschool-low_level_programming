c52476fb5e93e7827b0949775bab37b5cf81cb0a
Huang1810 <ahhamed1810@gmail.com>
2024-02-28 12:15:30 +0100

    print numbers

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,6 @@
 #include"main.h"
 /**
 * print_numbers - Prints number
-* @x: int
 * Return: Always 0.
 */
 void print_numbers(void)
@@ -11,5 +10,5 @@ for (x = 0; x <= 9; x++)
 {
 _putchar(x + '0');
 }
-_putchar('\n')
+_putchar('\n');
 }
