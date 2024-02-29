d740bdc2b9698e0f140b8b996dd6b68175b8b577
Huang1810 <ahhamed1810@gmail.com>
2024-02-29 10:47:20 +0100

    commit

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -7,7 +7,7 @@
 void more_numbers(void)
 {
 int x, y;
-for(x = 0; x < 10; x++)
+for (x = 0; x < 10; x++)
 {
 for (y = 0; y <= 14; y++)
 {
@@ -20,3 +20,4 @@ _putchar((y % 10) + '0');
 _putchar('\n');
 }
 }
+
