bc2d52f56f2416361dc628a2077f16dc15f8726d
Huang1810 <ahhamed1810@gmail.com>
2024-02-29 10:25:36 +0100

    commit

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -8,7 +8,7 @@ void print_most_numbers(void)
 int x = 0;
 for (; x <= 9; x++)
 {
-if (x == 2 || x == 4 )
+if (x == 2 || x == 4)
 {
 continue;
 }
@@ -19,3 +19,4 @@ _putchar(x + '0');
 }
 _putchar('\n');
 }
+
