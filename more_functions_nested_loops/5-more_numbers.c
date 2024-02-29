7dbf59af98e5c79709a56f7adee0cd572250e235
Huang1810 <ahhamed1810@gmail.com>
2024-02-29 10:42:03 +0100

    commit

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include"main.h"
+/**
+* print more_numbers - entry point
+* Description: Prints number
+* Return: Always 0.
+*/
+void more_numbers(void)
+{
+int x, y;
+for(x = 0; x < 10; x++)
+{
+for (y = 0; y <= 14; y++)
+{
+if (y > 9)
+{
+_putchar((y / 10) + '0');
+}
+_putchar((y % 10) + '0');
+}
+_putchar('\n')
+}
+}
+
