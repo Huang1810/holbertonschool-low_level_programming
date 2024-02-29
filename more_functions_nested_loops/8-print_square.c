a0f70329f06cc39f5dbaacc10aacedc21a37fb6b
Huang1810 <ahhamed1810@gmail.com>
2024-02-29 14:05:19 +0100

    commit

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include"main.h"
+/**
+* print_square - prints n squares
+* @n: the number of squares
+* Return: Always 0.
+*/
+void print_square(int size)
+{
+int x, y;
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (x = 0; x < n; x++)
+{
+for (y = 0; y < x; y++)
+{
+_putchar(35);
+}
+_putchar('\n');
+}
+}
+}
