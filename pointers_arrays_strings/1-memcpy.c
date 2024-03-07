63f6e466b4a0433a4984884bc1edc9e7c0ca232c
Huang1810 <ahhamed1810@gmail.com>
2024-03-07 12:31:22 +0100

    commit

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _memset - Entry point
+ * @dest: input
+ * @src: input
+ * @n: input
+ * Return: Always 0 (succes)
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+for (i = 0; i < n; i++)
+dest[i] = src[i];
+return (dest);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -5,3 +5,4 @@ void _puts(char *str);
 void print_rev(char *s);
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
