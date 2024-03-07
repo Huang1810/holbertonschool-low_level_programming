acd8361989dbbebce9c1fab52f764cd873bca412
Huang1810 <ahhamed1810@gmail.com>
2024-03-07 13:52:06 +0100

    commit

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strchr - Entry point
+ * @haystack: input
+ * @needle: input
+ * Description: locates a character in a string.
+ * Return: Always 0 (succes)
+ */
+char *_strstr(char *haystack, char *needle)
+{
+for (; *haystack != '\0'; haystack++)
+{
+char *one = haystack;
+char *two = needle;
+while (*one == *two && *two != '\0')
+{
+one++;
+two++;
+}
+if (*two == '\0')
+return (haystack);
+}
+return (NULL);
+}
