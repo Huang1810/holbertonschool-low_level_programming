4f5f20c1823de205599b7392c3d3495a5970f8b9
Huang1810 <ahhamed1810@gmail.com>
2024-03-08 10:49:49 +0100

    commit

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,9 @@
+#include"main.h"
+
+/**
+* _puts_recursion - Print a string followed by a new line
+* @s: the string to print
+*
+* Return: Nothing.
+*/
+int _sqrt_recursion(int n)
---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,9 @@
+#include"main.h"
+
+/**
+* _puts_recursion - Print a string followed by a new line
+* @s: the string to print
+*
+* Return: Nothing.
+*/
+int is_prime_number(int n)
