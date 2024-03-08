e792a0f2f3a76f6c8a0716f888395361f2af1902
Huang1810 <ahhamed1810@gmail.com>
2024-03-08 11:37:32 +0100

    commit

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -4,7 +4,6 @@
  * checker - checks the input number from n to the base
  * @n: number is squared and compared against base
  * @base: base number to check
- * Return: natural square root of number base
  * _sqrt_recursion - return the natural square root of a number n.
  * @n: number to check for square roots.
  * Return: the natural square root of number n
@@ -17,6 +16,13 @@ int checker(int n, int base)
 		return (-1);
 	return (checker(n + 1, base));
 }
+
+
+/**
+ * _sqrt_recursion - return the natural square root of a number n..
+ * @n: number is squared and compared against base
+ * Return: the natural square root of number n
+ */
 
 int _sqrt_recursion(int n)
 {
