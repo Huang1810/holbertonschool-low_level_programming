15089b1e812c68414f4f5558860290a062077b6c
Huang1810 <ahhamed1810@gmail.com>
2024-03-08 11:47:08 +0100

    commit

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -6,7 +6,7 @@
  */
 int is_prime_number(int n)
 {
-	return (helper_prime(n, 2, n / 2));
+return (helper_prime(n, 2, n / 2));
 }
 
 /**
@@ -19,10 +19,10 @@ int is_prime_number(int n)
 
 int helper_prime(int n, int i, int limit)
 {
-	if ((n % i == 0 && i <= limit) || n < 0 || n == 1)
-		return (0);
-	else if (n % i != 0 && i <= limit)
-		return (helper_prime(n, i + 1, limit));
-	else
-		return (1);
+if ((n % i == 0 && i <= limit) || n < 0 || n == 1)
+return (0);
+else if (n % i != 0 && i <= limit)
+return (helper_prime(n, i + 1, limit));
+else
+return (1);
 }
