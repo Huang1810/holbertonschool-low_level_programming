d530d9d1dd5733578ec9288bdaef910f4c28458a
Huang1810 <ahhamed1810@gmail.com>
2024-03-14 11:22:14 +0100

    commit

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -10,21 +10,21 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *ptr;
+	char *p;
 	unsigned int count;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	ptr = malloc(nmemb * size);
-	if (ptr == NULL)
+	p = malloc(nmemb * size);
+	if (p == NULL)
 	{
 		return (NULL);
 	}
 	count = 0;
 	while (count < nmemb * size)
 	{
-		ptr[count] = 0;
+		p[count] = 0;
 		count++;
 	}
-	return (ptr);
+	return (p);
 }
