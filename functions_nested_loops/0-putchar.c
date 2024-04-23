b1068553e05d3043996e12f3bce94fc0cb012169
Huang1810 <159008826+Huang1810@users.noreply.github.com>
2024-04-23 22:00:11 +0100

    Update 0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -6,14 +6,5 @@
 */
 int main(void)
 {
-_putchar('H');
-_putchar('o');
-_putchar('l');
-_putchar('b');
-_putchar('e');
-_putchar('r');
-_putchar('t');
-_putchar('o');
-_putchar('n');
 return (0);
 }
